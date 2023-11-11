4c4b300b13ebccc2852e5d8400612e067dd8276c
AchmadBaihaqi21 <achmad.baihaqi21@mhs.uinjkt.ac.id>
2023-11-11 00:58:29 -0500

    new navbar

--- script/index.js
+++ script/index.js
@@ -1,6 +1,1 @@
-const menuIcon = document.getElementById("menu-icon");
-const menuList = document.getElementById("menu-list");
-
-menuIcon.addEventListener("click", () => {
-  menuList.classList.toggle("hidden");
-});
+alert('Welcome To RBPedia');
